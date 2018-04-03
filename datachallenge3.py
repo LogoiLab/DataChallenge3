04b108ae52f26c103c2fa619505254a4acf35220
Chad Baxter <cbaxter@mail.umw.edu>
2018-04-02 22:37:28 -0400

    added data download for file if it doesn't exist

--- datachallenge3.py
+++ datachallenge3.py
@@ -1,18 +1,32 @@
 import pandas as pd
 import sqlite3
+import seaborn as sns
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import urllib
+import os.path
+
+fname = './res/Data/FPA_FOD_20170508.sqlite'
+if os.path.isfile(fname):
+    print("data found. not downloading.")
+else:
+    print("data not found. downloading.")
+    url = "https://nofile.io/f/8TwOWYnwGwW/FPA_FOD_20170508.sqlite"
+    urllib.request.urlretrieve(url, filename=fname)
+    print ("download complete!")
 
 #Establish database connection
-con = sqlite3.connect('res/Data/FPA_FOD_20170508.sqlite')
+con = sqlite3.connect(fname)
 
 # Get all column names for table Fires
-names = pd.read_sql("PRAGMA table_info(Fires)", con)  # Query
-print(names)  # Show
-del(names)  # Clear memory
+#names = pd.read_sql("PRAGMA table_info(Fires)", con)  # Query
+#print(names)  # Show
+#del(names)  # Clear memory
 
 # Get the distinct values for all columns in Fires
-distinct_vals = pd.read_sql("SELECT DISTINCT * FROM Fires", con)  # Query
-print(distinct_vals)  # Show
-del(distinct_vals)  # Clear memory
+#distinct_vals = pd.read_sql("SELECT DISTINCT * FROM Fires", con)  # Query
+#print(distinct_vals)  # Show
+#del(distinct_vals)  # Clear memory
 
 
 """
@@ -122,45 +136,54 @@ Important Columns:
 """
 
 # Get all the important columns
-important = pd.read_sql("SELECT SOURCE_SYSTEM_TYPE, SOURCE_SYSTEM, NWCG_REPORTING_AGENCY, NWCG_REPORTING_UNIT_NAME, FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, DISCOVERY_TIME, CONT_DATE, CONT_DOY, CONT_TIME, FIRE_SIZE, FIRE_SIZE_CLASS, LATITUDE, LONGITUDE, STATE, COUNTY, FIPS_CODE, FIPS_NAME FROM Fires", con)  # Query
-print(important.head())  # Show
-del(important)  # Clear memory
+#important = pd.read_sql("SELECT SOURCE_SYSTEM_TYPE, SOURCE_SYSTEM, NWCG_REPORTING_AGENCY, NWCG_REPORTING_UNIT_NAME, FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, DISCOVERY_TIME, CONT_DATE, CONT_DOY, CONT_TIME, FIRE_SIZE, FIRE_SIZE_CLASS, LATITUDE, LONGITUDE, STATE, COUNTY, FIPS_CODE, FIPS_NAME FROM Fires", con)  # Query
+#print(important.head())  # Show
+#del(important)  # Clear memory
 
 
 # Get the answers
-answers = pd.read_sql("SELECT STAT_CAUSE_CODE, STAT_CAUSE_DESCR FROM Fires", con)  # Query
-print(answers.head())  # Show
-del(answers)  # Clear memory
+#answers = pd.read_sql("SELECT STAT_CAUSE_CODE, STAT_CAUSE_DESCR FROM Fires", con)  # Query
+#print(answers.head())  # Show
+#del(answers)  # Clear memory
 
 
 # Get the location info
-location_data = pd.read_sql("SELECT LATITUDE, LONGITUDE, STATE, COUNTY, FIPS_CODE, FIPS_NAME FROM Fires", con)  # Query
-print(location_data.head())  # Show
-del(location_data)  # Clear memory
+#location_data = pd.read_sql("SELECT LATITUDE, LONGITUDE, STATE, COUNTY, FIPS_CODE, FIPS_NAME FROM Fires", con)  # Query
+#print(location_data.head())  # Show
+#del(location_data)  # Clear memory
 
 
 # Get the fire info
-fire_data = pd.read_sql("SELECT FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, DISCOVERY_TIME, CONT_DATE, CONT_DOY, CONT_TIME, FIRE_SIZE, FIRE_SIZE_CLASS FROM Fires", con)  # Query
-print(fire_data.head())  # Show
-del(fire_data)  # Clear memory
+#fire_data = pd.read_sql("SELECT FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, DISCOVERY_TIME, CONT_DATE, CONT_DOY, CONT_TIME, FIRE_SIZE, FIRE_SIZE_CLASS FROM Fires", con)  # Query
+#print(fire_data.head())  # Show
+#del(fire_data)  # Clear memory
 
 
 # Get the reporting info
-report_data = pd.read_sql("SELECT SOURCE_SYSTEM_TYPE, SOURCE_SYSTEM, NWCG_REPORTING_AGENCY, NWCG_REPORTING_UNIT_NAME FROM Fires", con)  # Query
-print(report_data.head())  # Show
-del(report_data)  # Clear memory
+#report_data = pd.read_sql("SELECT SOURCE_SYSTEM_TYPE, SOURCE_SYSTEM, NWCG_REPORTING_AGENCY, NWCG_REPORTING_UNIT_NAME FROM Fires", con)  # Query
+#print(report_data.head())  # Show
+#del(report_data)  # Clear memory
 
 
 # Get required fields
 required_data = pd.read_sql("SELECT FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, DISCOVERY_TIME, CONT_DATE, CONT_DOY, CONT_TIME, FIRE_SIZE, FIRE_SIZE_CLASS, STATE, COUNTY, FIPS_CODE, FIPS_NAME FROM Fires", con)  # Query
 print(required_data.head())  # Show
-# KEEP THIS DATA
+del(required_data)  # KEEP THIS DATA IN FINAL VERSION
 
 # TODO Clean data
 
 # TODO Convert dates to durations
 
 # TODO Chart stats for original data
+
+# Create size/year chart
+year_size = pd.read_sql("SELECT FIRE_YEAR, FIRE_SIZE FROM Fires", con)  # Query
+sns.regplot(x='FIRE_YEAR', y='FIRE_SIZE', data=year_size)  # Create chart
+plt.title('Fires/Year Sizes')
+plt.xlabel('Years')
+plt.ylabel('Fire sizes')
+plt.show()
+del(year_size) # Clear memory
 
 # TODO One-hot encode
 
