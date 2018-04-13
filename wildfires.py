a7cdf8e64237f34f2c81fdc79fdfd13add340d76
LordMilton <dufour.bradley@gmail.com>
2018-04-12 23:08:05 -0400

    Comments predict()

--- wildfires.py
+++ wildfires.py
@@ -44,7 +44,7 @@ con = sqlite3.connect(fname)
 #del(answers)  # Clear memory
 
 # Get required fields
-required_data = pd.read_sql("SELECT FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, DISCOVERY_TIME, CONT_DATE, CONT_DOY, CONT_TIME, FIRE_SIZE, FIRE_SIZE_CLASS, STATE, COUNTY, FIPS_CODE, STAT_CAUSE_DESCR FROM Fires limit 1000", con)  # Query
+required_data = pd.read_sql("SELECT FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, DISCOVERY_TIME, CONT_DATE, CONT_DOY, CONT_TIME, FIRE_SIZE, FIRE_SIZE_CLASS, STATE, COUNTY, FIPS_CODE, STAT_CAUSE_DESCR FROM Fires", con)  # Query
 required_data.dropna()
 
 # DONE Create durations.
@@ -70,9 +70,18 @@ clf.fit(features_train, target_train)
 target_pred = clf.predict(features_test)
 print("Testing dataset: ", str(sum(target_pred == target_test) / len(features_test)), "confidence." )
 
+# Predict's the cause of a wildfire based on the year it occurred, it's size
+#   class, the state
+#   in which it occurred, and it's duration in days
+# @param fire_year Year in which the fire occurred (must be an integer between
+#   1992 and 2005)
+# @param fire_size_class The size of the fire as a letter according to this
+#   [https://www.nwcg.gov/term/glossary/size-class-of-fire]
+# @param state The state the fire occurred in as a 2 letter abbreviation
+# @param dur_fire The length of the fire in days as an integer
 def predict(fire_year, fire_size_class, state, dur_fire):
     value = pd.DataFrame()
-    value['FIRE_YEAR'] = fire_year
+    value['FIRE_YEAR'] = fire_year - 1992
     value['FIRE_SIZE_CLASS'] = fire_size_class
     value['STATE'] = state
     value['DUR_FIRE'] = dur_fire
